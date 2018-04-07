6e3132ea479483ad3d29f0f07f2a0c919f0abd8d
BaykovaNadi <BaykovaNadi@yandex.ru>
2018-04-07 23:38:56 +0300

    1

--- src/client/app/components/Pokemon.jsx
+++ src/client/app/components/Pokemon.jsx
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types';
 
 export default class Pokemon extends PureComponent {
     static propTypes = {
-        id: PropTypes.string,
         url: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
     };
 
     render() {
         const { url, name } = this.props;
-        const urlFigure = `https://github.com/PokeAPI/sprites/tree/master/sprites/pokemon/&{id}.png`;
+        const id = url.split('\/')[6];
+        const urlFigure = `https://github.com/PokeAPI/sprites/tree/master/sprites/pokemon/${id}.png`;
 
         return (
             <div>
